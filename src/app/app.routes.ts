da63790abda7d462d8fb69174231c812e2b6ea1e
Malcom <malcom70976@gmail.com>
2024-09-20 18:53:45 +0000

    integration du herosection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
+import { LandingComponent } from './components/landing/landing.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: 'navbar', component:NavbarComponent},
+  { path: 'accueil', component:LandingComponent}
 
 
 ];
