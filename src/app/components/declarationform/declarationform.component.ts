de68b045e2eb13823f3ba6abc9a4e8e10c7d6b95
Malcom <malcom70976@gmail.com>
2024-09-23 14:04:44 +0000

    integration page publication et declaration

--- src/app/components/declarationform/declarationform.component.ts
+++ src/app/components/declarationform/declarationform.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-declarationform',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './declarationform.component.html',
   styleUrl: './declarationform.component.css'
 })
--- src/app/components/publishform/publishform.component.ts
+++ src/app/components/publishform/publishform.component.ts
@@ -1,9 +1,10 @@
+import { NavbarComponent } from './../navbar/navbar.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-publishform',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './publishform.component.html',
   styleUrl: './publishform.component.css'
 })
