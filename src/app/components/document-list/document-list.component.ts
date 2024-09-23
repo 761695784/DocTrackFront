413b45a0cb166db15e80506739bae5ec7c1a981d
Malcom <malcom70976@gmail.com>
2024-09-23 17:14:30 +0000

    integration page liste des publications

--- src/app/components/document-list/document-list.component.ts
+++ src/app/components/document-list/document-list.component.ts
@@ -1,12 +1,17 @@
+import { FooterComponent } from './../footer/footer.component';
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-document-list',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent,FooterComponent],
   templateUrl: './document-list.component.html',
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+
+  constructor() { }
+
 
 }
