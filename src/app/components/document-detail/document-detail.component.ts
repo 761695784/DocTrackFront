d5eef3c5c85ffa15d6fe537b61639531fc1da104
Malcom <malcom70976@gmail.com>
2024-09-26 11:57:29 +0000

    ajout de commentaire en marche

--- src/app/components/document-detail/document-detail.component.ts
+++ src/app/components/document-detail/document-detail.component.ts
@@ -1,9 +1,13 @@
+import { AuthService } from './../services/auth.service';
 import { FooterComponent } from './../footer/footer.component';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DetailsService } from '../services/details.service';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { CommentairesService } from '../services/commentaire.service';
+import { FormsModule } from '@angular/forms';
+
 
 // Assurez-vous d'utiliser une interface pour décrire les données du document
 export interface DocumentDetails {
@@ -28,18 +32,33 @@ export interface DocumentDetails {
     email_verified_at?: string;  // Optionnel, selon si c'est pertinent pour ton application
   };
 }
+export interface Commentaire {
+  id: number;
+  contenu: string;
+  document_id: number;
+  user: {
+    FirstName: string;
+    LastName: string;
+  };
+  created_at: string;
+}
 
 @Component({
   selector: 'app-document-detail',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, CommonModule],
+  imports: [NavbarComponent, FooterComponent, CommonModule,FormsModule],
   templateUrl: './document-detail.component.html',
   styleUrls: ['./document-detail.component.css']
 })
 export class DocumentDetailComponent implements OnInit {
   documentDetails: DocumentDetails | null = null; // Utiliser le type DocumentDetails
+  commentaires: Commentaire[] = []; // Ajout pour stocker les commentaires
+  newComment: string = ''; // Pour stocker le contenu du nouveau commentaire
 
-  constructor(private route: ActivatedRoute, private detailsService: DetailsService) { }
+  constructor(private route: ActivatedRoute, private detailsService: DetailsService,
+    private commentairesService: CommentairesService, // Ajout du service Commentaires
+    private authService: AuthService // Ajout du service AuthService
+  ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(paramMap => {
@@ -63,4 +82,35 @@ export class DocumentDetailComponent implements OnInit {
       error: (err) => console.error('Erreur lors de la récupération des détails du document', err)
     });
   }
+
+  getCommentaires(documentId: number): void {
+    this.commentairesService.getCommentairesByDocument(documentId).subscribe({
+      next: (comments) => {
+        this.commentaires = comments;
+      },
+      error: (err) => console.error('Erreur lors de la récupération des commentaires', err)
+    });
+  }
+
+  // Méthode pour ajouter un commentaire
+  addCommentaire(): void {
+    if (this.authService.isAuthenticated() && this.newComment.trim() !== '' && this.documentDetails?.id !== undefined) {
+      const commentaireData = {
+        contenu: this.newComment,
+        document_id: this.documentDetails!.id // Utilisation de l'opérateur non-null assertion '!'
+      };
+
+      this.commentairesService.addCommentaire(commentaireData).subscribe({
+        next: () => {
+          this.newComment = '';
+          this.getCommentaires(this.documentDetails?.id!); // Utilisation de l'opérateur non-null assertion '!'
+        },
+        error: (err) => console.error('Erreur lors de l\'ajout du commentaire', err)
+      });
+    } else {
+      console.error('Impossible d\'ajouter le commentaire car document_id est undefined ou l\'utilisateur n\'est pas authentifié');
+    }
+  }
+
+
 }
