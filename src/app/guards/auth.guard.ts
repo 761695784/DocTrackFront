2fe8d9ada83a2b5e4ffe5fd69e7e66629f9a2fa0
Malcom <malcom70976@gmail.com>
2024-09-29 18:11:41 +0000

    restriction des routes concernant le dashboard de l'administrateur

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -15,29 +15,26 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot): boolean {
 
     if (this.authService.isAuthenticated()) {
-      // Vérification du rôle Admin
       const user = JSON.parse(localStorage.getItem('user') || '{}');
       const roles = user.roles || [];
-
-      // Chercher si le rôle Admin existe
       const isAdmin = roles.some((role: any) => role.name === 'Admin');
 
-      console.log('Rôle de l\'utilisateur:', roles); // Vérifier tous les rôles
-      console.log('Est admin:', isAdmin); // Vérifier si l'utilisateur est admin
+      // Bloquer l'accès aux routes spécifiques si l'utilisateur n'est pas admin
+      const adminRoutes = ['admindec', 'admindetails/:id', 'adminpub', 'admin'];
+      const isRestrictedAdminRoute = adminRoutes.some(route => next.routeConfig?.path?.startsWith(route));
 
-      if (next.routeConfig?.path === 'admin' && !isAdmin) {
-        // Si l'utilisateur n'est pas admin, rediriger vers la page d'accueil
+      if (isRestrictedAdminRoute && !isAdmin) {
         this.router.navigate(['/accueil']);
         return false;
       }
 
       return true;
     } else {
-      // Stocker l'URL à laquelle l'utilisateur tentait d'accéder
       this.redirectService.setRedirectUrl(state.url);
-      this.router.navigate(['/connexion']); // Rediriger vers la page de connexion
+      this.router.navigate(['/connexion']);
       return false;
     }
   }
+
 
 }
