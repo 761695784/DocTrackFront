0dc3c663a5a1ccc6eef53ed0463ebe4494110a1e
Malcom <malcom70976@gmail.com>
2024-10-28 17:50:22 +0000

    ajout PWA et Service Worker pour la mise en place en mode PWA avec en cache les actifs statiques et les rendus disponibles hors ligne, améliorant ainsi les performances.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch()), // Activation du Fetch API
     provideAnimationsAsync() // Animations asynchrones
-  ]
+    ,
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
